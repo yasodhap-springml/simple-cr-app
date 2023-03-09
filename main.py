dba70d6b85f9d4921ccdd711e593ac29ee72d6cd
yasodhap-springml <yasodha.pudi@springml.com>
2023-03-09 08:18:06 +0530

    Bigquery change

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-
 import os
 
 from flask import Flask
+from google.cloud import bigquery
 
 app = Flask(__name__)
 
@@ -11,6 +10,17 @@ app = Flask(__name__)
 def hello_world():
     name = os.environ.get("NAME", "World")  + '. This is Backend Service'
     return "Hello {}!".format(name)
+
+def call_bq():
+    client = bigquery.Client()
+    query = """
+    SELECT * FROM `ashwani-21apr-22-scrumteam.cr_demo_dataset.cr_demo_table` LIMIT 1
+        """
+    query_job = client.query(query)  
+    for row in query_job:
+        row_name = row.name
+        #row_list.append(row_name)
+        print(row_name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
