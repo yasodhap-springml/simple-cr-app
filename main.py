33e360d38a4ee5d903b858c0cd444b021a852d3f
yasodhap-springml <yasodha.pudi@springml.com>
2023-07-20 14:38:56 +0530

    idtoekn changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
+import urllib
 
-from flask import Flask
+from google.auth.transport import requests
+from google.oauth2 import id_token
 import requests
+from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -10,13 +14,24 @@ app = Flask(__name__)
 def hello_frontend():
     # open a connection to a URL using urllib2
    #webUrl = urllib.request.urlopen("https://www.youtube.com/user/guru99com")
-    webUrl = requests.get("https://cr-demo-backend-6mckjolnxq-el.a.run.app")
+    audience = "https://fronend-service-n6rizrdx5q-uk.a.run.app"
+    endpoint = "https://backend-service-n6rizrdx5q-uk.a.run.app"
+    req = urllib.request.Request(endpoint)
+
+    auth_req = requests.Request()
+    idToken = id_token.fetch_id_token(auth_req, audience)
+    print(idToken)
+    req.add_header("Authorization", f"Bearer {idToken}")
+    response = urllib.request.urlopen(req)
+
+    #return response.read()
+   #webUrl = requests.get("https://cr-demo-backend-6mckjolnxq-el.a.run.app")
   
 #get the result code and print it
-    print ("result code: " + str(webUrl.status_code))
+    #print ("result code: " + str(response.read()))
   
 # read the data from the URL and print it
-    data = webUrl.text
+    data = response.read()
     print (data)
     return "This frontend service calling another service with message - {}!".format(data)
 
